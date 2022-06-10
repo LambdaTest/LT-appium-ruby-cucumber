e82741604b445664dcf63a0c78255d202c7f02ee
shashankj-lt <94521501+shashankj-lt@users.noreply.github.com>
2022-06-10 15:42:39 +0530

    Added multipleapps caps support
    Added multipleApps capability support

--- Android/examples/First_test/features/step_definitions/first_steps.rb
+++ Android/examples/First_test/features/step_definitions/first_steps.rb
@@ -25,37 +25,3 @@ When /^I try to Click on geoLocation using proverbial_android App/ do
   sleep 5
   $driver.back
 end
-
-When /^I try to Click on speedTest using proverbial_android App/ do
-  $wait.until {$driver.find_element(:id, "com.lambdatest.proverbial:id/speedTest").displayed?}
-  $driver.find_element(:id, "com.lambdatest.proverbial:id/speedTest").click
-  sleep 5
-  $driver.back
-end
-
-When /^I try to Click on Browser using proverbial_android App/ do
-  $wait.until {$driver.find_element(:id, "com.lambdatest.proverbial:id/Browser").displayed?}
-  $driver.find_element(:id, "com.lambdatest.proverbial:id/Browser").click
-end
-
-When /^I try to Click on url using proverbial_android App/ do
-  $wait.until {$driver.find_element(:id, "com.lambdatest.proverbial:id/url").displayed?}
-  $driver.find_element(:id, "com.lambdatest.proverbial:id/url").click
-end
-
-When("I type in {string}") do |search_keyword|
-  $wait.until { $driver.find_element(:id, "com.lambdatest.proverbial:id/url").displayed? }
-  search_box = $driver.find_element(:id, "com.lambdatest.proverbial:id/url")
-  search_box.send_key(search_keyword)
-  $wait.until {$driver.find_element(:id, "com.lambdatest.proverbial:id/find").displayed?}
-  $driver.find_element(:id, "com.lambdatest.proverbial:id/find").click
-  sleep 5
-end
-
-Then /^I should see results$/ do
-  $driver.find_elements(:class, "android.widget.TextView").size.should > 0
-end
-
-
-
-
